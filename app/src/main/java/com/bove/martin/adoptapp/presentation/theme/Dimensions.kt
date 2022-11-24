5f4043f55531611fdd1caf5cfe94a9fa65bc9227
mbove77 <mbove77@gmail.com>
2022-11-24 18:11:47 -0300

    Add compose theme.

---
+++ app/src/main/java/com/bove/martin/adoptapp/presentation/theme/Dimensions.kt
@@ -0,0 +1,13 @@
+package com.bove.martin.adoptapp.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by Martín Bove on 24/11/2022.
+ * E-mail: mbove77@gmail.com
+ */
+
+val smalSpace = 8.dp
+val normalSpace = 16.dp
+val largeSpace = 24.dp
+val extraLargeSpace = 32.dp
